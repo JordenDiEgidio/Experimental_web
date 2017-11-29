aadbc7052ee38b01e03e9ed65d6df41da5f40dfb
Mathias Dumortier <mathias@xumex.be>
2017-11-29 09:30:00 +0100

    storeImgSource API fix

--- src/js/lib/api/vision.js
+++ src/js/lib/api/vision.js
@@ -6,21 +6,25 @@ export default {
     const url = `https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBc2v6Bg6wnZJSxMGI3_rdJUubwu34HBB8`;
     const method = `POST`;
     const data =
+
+    //               imageUri: `https://cdn.pixabay.com/photo/2014/08/28/08/31/model-429733_960_720.jpg`
+
     {
-      request: [
+      requests: [
         {
           image: {
-            content: storeImgSource
+            content: storeImgSource,
           },
           features: [
             {
               type: `FACE_DETECTION`,
+
             }
           ]
         }
       ]
-    };
-
+    }
+    ;
     const body = JSON.stringify(data);
     return fetch(url, {method, body, "Content-Type": `application/json`})
       .then(r => r.json());
