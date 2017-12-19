e63b13c9b90fa8973899051d4687dcfbe733cbd2
Mathias Dumortier <mathias@xumex.be>
2017-12-19 13:08:48 +0100

    input field

--- src/js/components/textFilter/textField.jsx
+++ src/js/components/textFilter/textField.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import {observer, inject} from "mobx-react";
-import {func} from "prop-types";
+import {func, string} from "prop-types";
 
-const textColor = ({setSelectedColor}) => {
-  // const handleSelectedColor = e => {
-  //   const color = e.target.dataset.color;
-  //   console.log(`selected ${  color}`);
-  //   setSelectedColor(color);
-  // };
+const textField = ({setSelectedLabel, selectedLabel}) => {
+
+  const handleSetLabel = e => {
+    setSelectedLabel(e.target.value);
+  };
   return (
     <div>
-      <h2>Type your own text</h2>
-      <form className='color-group'>
-        <input type='text' />
-        <input type='submit' />
-      </form>
+      <h2>Custom label</h2>
+      <input value={selectedLabel} onChange={handleSetLabel} type='text' />
     </div>
   );
 
 };
 
-textColor.propTypes = {
-  setSelectedColor: func.isRequired,
+textField.propTypes = {
+  setSelectedLabel: func.isRequired,
+  selectedLabel: string.isRequired,
+
 };
 
 export default inject(
   ({store}) => {
-    return {setSelectedColor: store.setSelectedColor};
+    return {
+      setSelectedLabel: store.setSelectedLabel,
+      selectedLabel: store.selectedLabel
+    };
   }
 )(
-  observer(textColor)
+  observer(textField)
 );
--- src/js/components/textFilter/textFilter.jsx
+++ src/js/components/textFilter/textFilter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {object} from "prop-types";
 import {observer, inject} from "mobx-react";
 import TextfilterElement from './textFilterElement.jsx';
+import TextField from './textField.jsx';
 import TextColor from './textColor.jsx';
 import Dropdown from '../dropdown/dropdown.jsx';
 import Slider from '../dropdown/slider.jsx';
@@ -13,6 +14,7 @@ const textFilter = ({labels}) => {
 
   return (
     <div className='edit'>
+      <TextField />
       <h2>Smart captions</h2>
       {
         labels.map(
